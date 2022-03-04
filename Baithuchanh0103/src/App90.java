a95fe8730f4844300d29ad7270cf26defeb3a898
theanhb12 <theanhb123092002@gmail.com>
2022-03-04 21:32:49 +0700

    baithuchanh0301

---
+++ Baithuchanh0103/src/App90.java
@@ -0,0 +1,18 @@
+package src;
+import java.util.Scanner;
+
+public class App90 {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("nhap chuoi: ");
+        String s = sc.next();
+        char kytu;
+        System.out.println("Cac ky tu co trong chuoi la: ");
+        for (int i = 0; i < s.length(); i++) {
+            kytu = s.charAt(i);    
+            System.out.println(kytu);
+            sc.close();
+        }
+    }
+}
+
