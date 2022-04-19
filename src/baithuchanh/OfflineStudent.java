f77d11e3cb9f2a7c810d34b6197012311fcf47bd
Theanh2309 <97503289+Theanh2309@users.noreply.github.com>
2022-04-19 14:56:59 +0700

    Delete src directory

--- src/baithuchanh/OfflineStudent.java
+++
@@ -1,9 +0,0 @@
-package baithuchanh;
-
-    public class OfflineStudent extends Student{
-       // public boolean chuyencan;
-
-        public void chuyencan(String name){
-            System.out.println("Hoc sinh" +name+"nghi hoc");
-        }
-    }
--- src/baithuchanh/OnlineStudent.java
+++
@@ -1,7 +0,0 @@
-package baithuchanh;
-
-public class OnlineStudent extends Student {
-    public void chuyencan(String name){
-        System.out.println("Hoc sinh" +name+"dang hoc trong lop");
-    }
-}
